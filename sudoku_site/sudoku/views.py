bd0e6ff910a90249da61add7c008b781b3f72a3e
Katarina Kovacova <katarinakovacova100@gmail.com>
2024-06-14 10:05:48 +0200

    Basic frontend and refactoring of project structure

---
+++ sudoku_site/sudoku/views.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+
+from sudoku.puzzle import make_new_puzzle
+
+from django.shortcuts import render
+
+
+def puzzle(request):
+    n_attempts, puzzle =  make_new_puzzle()
+
+    output = {
+        'n_attempts': n_attempts,
+        'puzzle' : puzzle
+    }
+
+    return JsonResponse(output)
+
+def home(request):
+    return render(request, 'home.html')
+
+def home(request):
+    n_attempts, sudoku_puzzle = make_new_puzzle()   
+    context = {
+        'sudoku_puzzle': sudoku_puzzle,
+        'n_attempts': n_attempts, 
+    }
+    return render(request, 'home.html', context)
